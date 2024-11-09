2e290efe9deaa9c2b0167408035924e21560d180
Moresko <46751543+Moresko@users.noreply.github.com>
2024-11-09 12:03:55 +0100

    reservation
    media q is far future

--- autoflex/src/components/reservation.js
+++ autoflex/src/components/reservation.js
@@ -1,11 +1,23 @@
 import React from "react";
 import '../css/reservation.css';
 import gt from '../images/greenTire.png';
+import { RiPulseFill } from 'react-icons/ri';
 
 const Header = () => {
     return (
         <div className="reservation">
             <img src={gt} alt="Zelene pozadie" className="background" />
+            <div className="content">
+                <div className="text-rec">
+                    <div className="reservation-header">
+                        <RiPulseFill className="icon" />
+                        <h4>OBJEDNAJTE SA</h4>
+                    </div>
+                    <h2>DOSTANEME VÁS SPÄŤ NA CESTU</h2>
+                    <p>Rezervujte sa telefonicky, emailom alebo využite náš online rezervačný formulár a rezervujte termín podľa svojich potrieb. Vyberte dátum a čas, ktorý vám vyhovuje, a my sa o vaše vozidlo postaráme.</p>
+                </div>
+                <div className="black-rectangle"></div>
+            </div>
         </div>
     );
 };
