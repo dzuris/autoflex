62b738bfdbbf6f75beb3297ba1c8a4ddb5750a8e
Moresko <46751543+Moresko@users.noreply.github.com>
2024-11-11 08:22:47 +0100

    icons move

--- autoflex/src/components/reservation.js
+++ autoflex/src/components/reservation.js
@@ -2,6 +2,9 @@ import React from "react";
 import '../css/reservation.css';
 import gt from '../images/greenTire.png';
 import { RiPulseFill } from 'react-icons/ri';
+import { FaLocationDot } from "react-icons/fa6";
+import {LuPhoneCall} from 'react-icons/lu'
+import { FaRegEnvelope } from "react-icons/fa";
 
 const Header = () => {
     return (
@@ -15,6 +18,17 @@ const Header = () => {
                     </div>
                     <h2>DOSTANEME VÁS SPÄŤ NA CESTU</h2>
                     <p>Rezervujte sa telefonicky, emailom alebo využite náš online rezervačný formulár a rezervujte termín podľa svojich potrieb. Vyberte dátum a čas, ktorý vám vyhovuje, a my sa o vaše vozidlo postaráme.</p>
+                    <div className="allIcons">
+                        <div className="iconWrapper">
+                            <FaLocationDot className="locationIcon"/>
+                        </div>
+                        <div className="iconWrapper">
+                            <LuPhoneCall className="phoneIcon"/>
+                        </div>
+                        <div className="iconWrapper">
+                            <FaRegEnvelope className="envelopeIcon" />
+                        </div>
+                    </div>
                 </div>
                 <div className="black-rectangle"></div>
             </div>
